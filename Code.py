c0ffc168a808b4d7ee12aa839789904cde952913
suhaas30 <76256483+suhaas30@users.noreply.github.com>
2022-01-03 15:16:20 +0300

    Add files via upload

---
+++ Code.py
@@ -0,0 +1,28 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter a number: ")
+
+result = float(num1) + float(num2)
+
+print(result)
+
+num1 = input("Enter a number: ")
+num2 = input("Enter a number: ")
+
+result = float(num1) - float(num2)
+
+print(result)
+
+num1 = input("Enter a number: ")
+num2 = input("Enter a number: ")
+
+result = float(num1) * float(num2)
+
+print(result)
+
+num1 = input("Enter a number: ")
+num2 = input("Enter a number: ")
+
+result = float(num1) / float(num2)
+
+print(result)
+
